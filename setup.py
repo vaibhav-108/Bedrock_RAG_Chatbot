b8c393a20206b551a92090fc540389d67f54cf43
vaibhav-108 <vaibhav.b108@gmail.com>
2025-04-27 16:14:19 +0530

    Q&A tested

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="vaibhav-108",
     author_email="vaibhav.b108@gmail.com",
     packages=find_packages(),
-    install_requires=["langchain","langchainhub","bs4","tiktoken","openai","boto3==1.38.3","langchain_community","chromadb","awscli",
+    install_requires=["langchain","langchainhub","langchain_aws","bs4","tiktoken","openai","boto3==1.38.3","langchain_community","chromadb","awscli",
 "streamlit",
 "pypdf",
 "faiss-cpu"]
